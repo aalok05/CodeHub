d2dde24ddd94f60eda0ee99e4abf550b67a3b7b4
Aalok Singh <aalok_2@live.com>
2017-01-26 09:40:13 +0530

    Added SettingsItem class

---
+++ CodeHub/Models/SettingsItem.cs
@@ -0,0 +1,45 @@
+﻿using GalaSoft.MvvmLight;
+using Windows.UI.Xaml.Documents;
+
+namespace CodeHub.Models
+{
+    public class SettingsItem : ObservableObject
+    {
+        public string _mainText;
+        public string MainText
+        {
+            get
+            {
+                return _mainText;
+            }
+            set
+            {
+                Set(() => MainText, ref _mainText, value);
+            }
+        }
+        public string _subText;
+        public string SubText
+        {
+            get
+            {
+                return _subText;
+            }
+            set
+            {
+                Set(() => SubText, ref _subText, value);
+            }
+        }
+        public string _glyphString;
+        public string GlyphString
+        {
+            get
+            {
+                return _glyphString;
+            }
+            set
+            {
+                Set(() => GlyphString, ref _glyphString, value);
+            }
+        }
+    }
+}
