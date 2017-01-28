5efdcde7d9950e300651937f099ef983f8ef6c20
Aalok Singh <aalok_2@live.com>
2017-01-28 17:35:43 +0530

    Handled store purchase result

--- CodeHub/Views/DonateView.xaml.cs
+++ CodeHub/Views/DonateView.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Services.Store;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -37,18 +38,57 @@ namespace CodeHub.Views
         private async void first_tier_Tapped(object sender, TappedRoutedEventArgs e)
         {
             StorePurchaseResult result = await WindowsStore.RequestPurchaseAsync(donateFirstAddOnId);
+            reactToPurchaseResult(result);
         }
         private async void second_tier_Tapped(object sender, TappedRoutedEventArgs e)
         {
             StorePurchaseResult result = await WindowsStore.RequestPurchaseAsync(donateSecondAddOnId);
+            reactToPurchaseResult(result);
         }
         private async void third_tier_Tapped(object sender, TappedRoutedEventArgs e)
         {
             StorePurchaseResult result = await WindowsStore.RequestPurchaseAsync(donateThirdAddOnId);
+            reactToPurchaseResult(result);
         }
         private async void fourth_tier_Tapped(object sender, TappedRoutedEventArgs e)
         {
             StorePurchaseResult result = await WindowsStore.RequestPurchaseAsync(donateFourthAddOnId);
+            reactToPurchaseResult(result);
+        }
+
+        private async void reactToPurchaseResult(StorePurchaseResult result)
+        {
+            if(result.Status == StorePurchaseStatus.Succeeded)
+            {
+
+                var messageDialog = new MessageDialog("Thanks for your donation! I deeply appreciate your contribution to the development of CodeHub.");
+
+                messageDialog.Commands.Add(new UICommand("OK"));
+
+                messageDialog.CancelCommandIndex = 0;
+
+                await messageDialog.ShowAsync();
+            }
+            else if(result.Status == StorePurchaseStatus.AlreadyPurchased)
+            {
+                var messageDialog = new MessageDialog("It seems you have already made this donation.");
+
+                messageDialog.Commands.Add(new UICommand("OK"));
+
+                messageDialog.CancelCommandIndex = 0;
+
+                await messageDialog.ShowAsync();
+            }
+            else
+            {
+                var messageDialog = new MessageDialog("There seems to be a problem. Try again later.");
+
+                messageDialog.Commands.Add(new UICommand("OK"));
+
+                messageDialog.CancelCommandIndex = 0;
+
+                await messageDialog.ShowAsync();
+            }
         }
     }
 }
