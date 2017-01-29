3ec327bde5671bc2c507d34f878ed6e484b0744c
Aalok Singh <aalok_2@live.com>
2017-01-29 18:01:28 +0530

    Removed AboutSettingsViewmodel.cs
    All settings based views will have a single ViewModel:
    SettingsViewmodel.cs

--- CodeHub/Views/AboutSettingsView.xaml.cs
+++ CodeHub/Views/AboutSettingsView.xaml.cs
@@ -7,14 +7,14 @@ using Windows.UI.Xaml.Controls;
 
 namespace CodeHub.Views
 {
-    public sealed partial class AboutView : SettingsDetailPageBase
+    public sealed partial class AboutSettingsView : SettingsDetailPageBase
     {
-        private AboutViewmodel ViewModel;
-        public AboutView()
+        private SettingsViewModel ViewModel;
+        public AboutSettingsView()
         {
             this.InitializeComponent();
 
-            ViewModel = new AboutViewmodel();
+            ViewModel = new SettingsViewModel();
             this.DataContext = ViewModel;
         }
         private void OnCurrentStateChanged(object sender, VisualStateChangedEventArgs e)
