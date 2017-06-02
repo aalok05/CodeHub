8d5254b6d981e11b6a89d7f30bf3b872d9f62e73
Aalok Singh <aalok_2@live.com>
2017-06-02 20:23:18 +0530

    Cleanup

--- CodeHub/Services/RepositoryUtility.cs
+++ CodeHub/Services/RepositoryUtility.cs
@@ -697,6 +697,24 @@ namespace CodeHub.Services
             }
         }
 
+        /// <summary>
+        /// Creates a new Issue for a repository
+        /// </summary>
+        /// <param name="repoId"></param>
+        /// <param name="newIssue"></param>
+        /// <returns></returns>
+        public static async Task<Issue> CreateIssue(long repoId, NewIssue newIssue)
+        {
+            try
+            { 
+                var client = await UserUtility.GetAuthenticatedClient();
+                return await client.Issue.Create(repoId, newIssue);
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
 
