0c8c8a41f75df0690b97f3331a3ff4b2f461cd8b
Aalok Singh <aalok_2@live.com>
2017-06-08 19:59:05 +0530

    Added Labels control to IssueDetailView

---
+++ CodeHub/Converters/ColorStringToColorBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using CodeHub.Helpers;
+
+namespace CodeHub.Converters
+{
+    class ColorStringToColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return GlobalHelper.GetSolidColorBrush((value as string)+"FF");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
