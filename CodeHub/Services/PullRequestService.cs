71ddeb96623e546241b629c0896b3c38157fba87
Aalok Singh <aalok_2@live.com>
2017-06-11 00:27:17 +0530

    Added PullRequestService class

---
+++ CodeHub/Services/PullRequestService.cs
@@ -0,0 +1,32 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHub.Services
+{
+    class PullRequestService
+    {
+        /// <summary>
+        /// Creates a new comment on a specified Pull Request
+        /// </summary>
+        /// <param name="repoId"></param>
+        /// <param name="number"></param>
+        /// <param name="comment"></param>
+        /// <returns></returns>
+        public static async Task<PullRequestReviewComment> CommentOnPullRequest(long repoId, int number, PullRequestReviewCommentCreate comment)
+        {
+            try
+            {
+                var client = await UserUtility.GetAuthenticatedClient();
+                return await client.PullRequest.ReviewComment.Create(repoId, number, comment);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
