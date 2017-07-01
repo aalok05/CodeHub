9a6b9523c0f93a4852cc3d20242ca18061618546
Aalok Singh <aalok_2@live.com>
2017-07-01 19:21:55 +0530

    Modfied PR and Issue ListItems

---
+++ CodeHub/Converters/IssueDetailStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Octokit;
+using CodeHub.Helpers;
+
+namespace CodeHub.Converters
+{
+    class IssueDetailStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Issue issue = (Issue)value;
+            switch (issue.State)
+            {
+                case ItemState.Open:
+                    return $"#{issue.Number} opened by {issue.User.Login} {GlobalHelper.ConvertDateToTimeAgoFormat(DateTime.Parse(issue.CreatedAt.ToString()))}";
+
+                case ItemState.Closed:
+                    return $"#{issue.Number} by {issue.User.Login} was closed {GlobalHelper.ConvertDateToTimeAgoFormat(DateTime.Parse(issue.CreatedAt.ToString()))}";
+
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CodeHub/Helpers/GlobalHelper.cs
+++ CodeHub/Helpers/GlobalHelper.cs
@@ -128,6 +128,52 @@ namespace CodeHub.Helpers
             //var osVersion = $"{major}.{minor}.{build}.{revision}";
             return build;
         }
+
+        /// <summary>
+        /// Coverts a DateTime to 'time ago' format
+        /// </summary>
+        /// <returns></returns>
+        public static string ConvertDateToTimeAgoFormat(DateTime dt)
+        {
+            var ts = new TimeSpan(DateTime.Now.Ticks - dt.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
+
+            if (delta < 60)
+            {
+                return ts.Seconds == 1 ? "one second ago" : ts.Seconds + " seconds ago";
+            }
+            if (delta < 120)
+            {
+                return "a minute ago";
+            }
+            if (delta < 2700) // 45 * 60
+            {
+                return ts.Minutes + " minutes ago";
+            }
+            if (delta < 5400) // 90 * 60
+            {
+                return "an hour ago";
+            }
+            if (delta < 86400) // 24 * 60 * 60
+            {
+                return ts.Hours + " hours ago";
+            }
+            if (delta < 172800) // 48 * 60 * 60
+            {
+                return "a day ago";
+            }
+            if (delta < 2592000) // 30 * 24 * 60 * 60
+            {
+                return ts.Days + " days ago";
+            }
+            if (delta < 31104000) // 12 * 30 * 24 * 60 * 60
+            {
+                int months = System.Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "one month ago" : months + " months ago";
+            }
+            int years = System.Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+            return years <= 1 ? "one year ago" : years + " years ago";
+        }
     }
 
 }
