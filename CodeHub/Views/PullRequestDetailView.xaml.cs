0ed673324015afdd40d6bf647dfe7b69a2cb23d0
Aalok Singh <aalok_2@live.com>
2017-09-03 14:10:53 +0530

    markdown support for commenting on PRs

--- CodeHub/Views/PullRequestDetailView.xaml.cs
+++ CodeHub/Views/PullRequestDetailView.xaml.cs
@@ -3,7 +3,12 @@ using CodeHub.ViewModels;
 using GalaSoft.MvvmLight.Messaging;
 using Octokit;
 using System;
+using System.Threading.Tasks;
+using UICompositionAnimations;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml;
+using UICompositionAnimations.Enums;
 
 namespace CodeHub.Views
 {
@@ -40,14 +45,16 @@ namespace CodeHub.Views
             }
         }
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
         {
+            base.OnNavigatedTo(e);
+
             if (e.NavigationMode == NavigationMode.Back)
             {
                 NavigationCacheMode = NavigationCacheMode.Disabled;
             }
 
-            base.OnNavigatedTo(e);
+            await ToggleCommentDialogVisibility(false);
         }
 
         public void ConfigureStateSymbol(PullRequest pr)
@@ -65,6 +72,46 @@ namespace CodeHub.Views
 
                 //closed PR
                 stateSymbol.Data = GlobalHelper.GetGeomtery("M11 11.28V5c-.03-.78-.34-1.47-.94-2.06C9.46 2.35 8.78 2.03 8 2H7V0L4 3l3 3V4h1c.27.02.48.11.69.31.21.2.3.42.31.69v6.28A1.993 1.993 0 0 0 10 15a1.993 1.993 0 0 0 1-3.72zm-1 2.92c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zM4 3c0-1.11-.89-2-2-2a1.993 1.993 0 0 0-1 3.72v6.56A1.993 1.993 0 0 0 2 15a1.993 1.993 0 0 0 1-3.72V4.72c.59-.34 1-.98 1-1.72zm-.8 10c0 .66-.55 1.2-1.2 1.2-.65 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z");
+            }
+        }
+
+        private async void CommentDialogOpen_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await ToggleCommentDialogVisibility(true);
+        }
+
+        private void EditZone_TextChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ViewModel.CommentText = Toolbar.Formatter?.Text;
+        }
+
+        private async void CancelComment_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await ToggleCommentDialogVisibility(false);
+        }
+
+        private async void Comment_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(ViewModel.CommentText))
+            {
+                await ToggleCommentDialogVisibility(false);
+                ViewModel.CommentCommand.Execute(null);
+            }
+        }
+
+        private async Task ToggleCommentDialogVisibility(bool visible)
+        {
+            if (visible)
+            {
+                CommentDialog.SetVisualOpacity(0);
+                CommentDialog.Visibility = Visibility.Visible;
+                await CommentDialog.StartCompositionFadeScaleAnimationAsync(0, 1, 1.1f, 1, 150, null, 0, EasingFunctionNames.SineEaseInOut);
+                EditZone.Focus(FocusState.Programmatic);
+            }
+            else
+            {
+                await CommentDialog.StartCompositionFadeScaleAnimationAsync(1, 0, 1, 1.1f, 150, null, 0, EasingFunctionNames.SineEaseInOut);
+                CommentDialog.Visibility = Visibility.Collapsed;
             }
         }
     }
