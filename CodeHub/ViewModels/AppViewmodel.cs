e494f5d05eac0fd9ad8dfd3f306bfaaccfca128b
Aalok Singh <aalok_2@live.com>
2018-03-05 13:04:21 +0530

    version update

--- CodeHub/ViewModels/AppViewmodel.cs
+++ CodeHub/ViewModels/AppViewmodel.cs
@@ -91,7 +91,7 @@ namespace CodeHub.ViewModels
         {
             get
             {
-                return "New Year New Update! \nChangelog for v2.4.6\n\x2022 Bug fixes and various improvements under the hood";
+                return "Hi all! \nHere's the changelog for v2.4.8\n\n\x2022 Under the hood improvements for upcoming updates.";
             }
         }
         #endregion
