78ea4aa0da6659735976550dd09035c02a7ebfed
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-09-18 23:26:41 +0600

    Code Fix

---
+++ CodeHub/Views/Settings/SettingsDetailPageBase.cs
@@ -0,0 +1,18 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+
+namespace CodeHub.Views.Settings
+{
+	public class SettingsDetailPageBase : Windows.UI.Xaml.Controls.Page
+	{
+		public void TryNavigateBackForDesktopState(string stateName)
+		{
+			if (stateName == "Desktop")
+			{
+				if (SimpleIoc.Default.GetInstance<Services.IAsyncNavigationService>().CurrentSourcePageType != typeof(SettingsView))
+				{
+					SimpleIoc.Default.GetInstance<Services.IAsyncNavigationService>().GoBackAsync();
+				}
+			}
+		}
+	}
+}
