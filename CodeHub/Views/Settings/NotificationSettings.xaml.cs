74d5072b1524c9b7286294c66969846bca9e3b3d
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-09-18 23:27:38 +0600

    Add NotificationSetting View, ViewModel

---
+++ CodeHub/Views/Settings/NotificationSettings.xaml.cs
@@ -0,0 +1,27 @@
+﻿using CodeHub.ViewModels.Settings;
+using Windows.UI.Xaml;
+
+namespace CodeHub.Views.Settings
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class NotificationSettings : SettingsDetailPageBase
+	{
+		private NofiticationSettingsViewModel ViewModel;
+
+		public NotificationSettings()
+		{
+			InitializeComponent();
+
+			ViewModel = new NofiticationSettingsViewModel();
+
+			DataContext = ViewModel;
+		}
+		private void OnCurrentStateChanged(object sender, VisualStateChangedEventArgs e)
+		{
+			if (e.NewState != null)
+				TryNavigateBackForDesktopState(e.NewState.Name);
+		}
+	}
+}
