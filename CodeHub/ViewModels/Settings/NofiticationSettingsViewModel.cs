4142ba5bff4438150198811a4a422c86ee852c0b
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-09-22 04:43:48 +0600

    Update NotificationSettingsVM for LiveTiles settings

--- CodeHub/ViewModels/Settings/NofiticationSettingsViewModel.cs
+++ CodeHub/ViewModels/Settings/NofiticationSettingsViewModel.cs
@@ -3,27 +3,80 @@ using GalaSoft.MvvmLight;
 
 namespace CodeHub.ViewModels.Settings
 {
-	public class NofiticationSettingsViewModel : ObservableObject
-	{
-		private bool _isToastEnabled = SettingsService.Get<bool>(SettingsKeys.IsToastEnabled);
+    public class NofiticationSettingsViewModel : ObservableObject
+    {
+        private bool _isToastEnabled = SettingsService.Get<bool>(SettingsKeys.IsToastEnabled);
+        private bool _isLiveTilesEnabled = SettingsService.Get<bool>(SettingsKeys.IsLiveTilesEnabled);
+        private bool _isLiveTilesBadgeEnabled = SettingsService.Get<bool>(SettingsKeys.IsLiveTilesBadgeEnabled);
+        private bool _isLiveTileUpdateAllBadgesEnabled = SettingsService.Get<bool>(SettingsKeys.IsLiveTileUpdateAllBadgesEnabled);
 
-		public bool IsToastEnabled
-		{
-			get => _isToastEnabled;
-			set
-			{
-				if (_isToastEnabled != value)
-				{
-					_isToastEnabled = value;
-					SettingsService.Save(SettingsKeys.IsToastEnabled, value);
-					RaisePropertyChanged();
-				}
-			}
-		}
+        public bool IsToastEnabled
+        {
+            get => _isToastEnabled;
+            set
+            {
+                if (_isToastEnabled != value)
+                {
+                    _isToastEnabled = value;
+                    SettingsService.Save(SettingsKeys.IsToastEnabled, value);
+                    RaisePropertyChanged(() => IsToastEnabled);
+                }
+            }
+        }
 
-		public NofiticationSettingsViewModel()
-		{
+        public bool IsLiveTilesEnabled
+        {
+            get => _isLiveTilesEnabled;
+            set
+            {
+                if (_isLiveTilesEnabled != value)
+                {
+                    _isLiveTilesEnabled = value;
+                    SettingsService.Save(SettingsKeys.IsLiveTilesEnabled, value);
+                    RaisePropertyChanged(() => IsLiveTilesEnabled);
+                }
+                if (!value && IsLiveTilesBadgeEnabled)
+                {
+                    IsLiveTilesBadgeEnabled = false;
+                }
+            }
+        }
 
-		}
-	}
+        public bool IsLiveTilesBadgeEnabled
+        {
+            get => _isLiveTilesBadgeEnabled;
+            set
+            {
+                if (_isLiveTilesBadgeEnabled != value)
+                {
+                    _isLiveTilesBadgeEnabled = value;
+                    SettingsService.Save(SettingsKeys.IsLiveTilesBadgeEnabled, value);
+                    RaisePropertyChanged(() => IsLiveTilesBadgeEnabled);
+                }
+                if (!value && IsAllBadgesUpdateEnabled)
+                {
+                    IsAllBadgesUpdateEnabled = false;
+                }
+            }
+        }
+
+        public bool IsAllBadgesUpdateEnabled
+        {
+            get => _isLiveTilesEnabled;
+            set
+            {
+                if (_isLiveTileUpdateAllBadgesEnabled != value)
+                {
+                    _isLiveTileUpdateAllBadgesEnabled = value;
+                    SettingsService.Save(SettingsKeys.IsLiveTileUpdateAllBadgesEnabled, value);
+                    RaisePropertyChanged(() => IsAllBadgesUpdateEnabled);
+                }
+            }
+        }
+
+        public NofiticationSettingsViewModel()
+        {
+
+        }
+    }
 }
