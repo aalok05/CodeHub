9ad2b8adb182d5b1ccde9a72461897ae1b0a2fd3
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-10-08 07:15:47 +0600

    All task registered tasks except maintenance and time trigger will be unregistered upon new registration

--- CodeHub/Helpers/BackgroundTaskHelper.cs
+++ CodeHub/Helpers/BackgroundTaskHelper.cs
@@ -9,11 +9,28 @@ namespace CodeHub.Helpers
         public static BackgroundTaskBuilder BuildBackgroundTask(string name, IBackgroundTrigger trigger, params IBackgroundCondition[] conditions)
         {
             BackgroundTaskBuilder builder = null;
+
             var taskExists = BackgroundTaskRegistration.AllTasks.Any(i => i.Value.Name == name);
+            TimeTrigger timeTrigger = null;
+            MaintenanceTrigger maintenanceTrigger = null;
+
             if (taskExists)
             {
-                var taskDic = BackgroundTaskRegistration.AllTasks.Single(i => i.Value.Name == name);
-                taskDic.Value.Unregister(true);
+                if (trigger is TimeTrigger tTrigger)
+                {
+                    timeTrigger = tTrigger;
+                }
+
+                if (trigger is MaintenanceTrigger mTrigger)
+                {
+                    maintenanceTrigger = mTrigger;
+                }
+
+                if (timeTrigger == null && maintenanceTrigger == null)
+                {
+                    var taskDic = BackgroundTaskRegistration.AllTasks.SingleOrDefault(i => i.Value.Name == name);
+                    taskDic.Value.Unregister(true);
+                }
             }
 
             // Specify the background task
@@ -22,7 +39,7 @@ namespace CodeHub.Helpers
                 Name = name
             };
 
-            // Set the trigger for Listener, listening to Toast Notifications
+            // Set the trigger for Listener
             builder.SetTrigger(trigger);
 
             if (conditions != null && conditions.Length > 0)
@@ -48,6 +65,12 @@ namespace CodeHub.Helpers
         {
             var builder = BuildBackgroundTask(name, trigger, conditions);
             builder.TaskEntryPoint = typeof(T).FullName;
+            return builder;
+        }
+        public static BackgroundTaskBuilder BuildBackgroundTask(string name, string entryPointName, IBackgroundTrigger trigger, params IBackgroundCondition[] conditions)
+        {
+            var builder = BuildBackgroundTask(name, trigger, conditions);
+            builder.TaskEntryPoint = entryPointName;
             return builder;
         }
     }
